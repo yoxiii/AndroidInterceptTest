a5e465b769a574677402f199c351ae5106d6d7cf
My Name <578228211@qq.com>
2023-11-01 17:45:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/intercepttest/MyHelper.java
@@ -0,0 +1,24 @@
+package com.example.intercepttest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyHelper extends SQLiteOpenHelper {
+
+    private final String db_name="yoxi.db";
+    private final int version=1;
+    public MyHelper(Context context) {
+        super(context, "yoxi.db",null,2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE call(_id INTEGER PRIMARY KEY AUTOINCREMENT,phonenum VARCHAR(20),time VARCHAR(40))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
